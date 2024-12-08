d8faabdc2eb5235fdbec12e4a5dc0fb7fa4fdac4
erikalien5595 <li_jiyanglin@163.com>
2024-12-08 22:06:37 +0800

    修改itransformer脚本，实验L=336的case

---
+++ scripts/multivariate_forecasting/PEMS/PEMS_all.sh
@@ -0,0 +1,4 @@
+bash ./scripts/multivariate_forecasting/PEMS/iTransformer_03.sh
+bash ./scripts/multivariate_forecasting/PEMS/iTransformer_04.sh
+bash ./scripts/multivariate_forecasting/PEMS/iTransformer_07.sh
+bash ./scripts/multivariate_forecasting/PEMS/iTransformer_08.sh
